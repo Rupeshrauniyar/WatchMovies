d363e5a1dabace7e38e660ab0967e64178376120
Rupeshrauniyar <151705269+Rupeshrauniyar@users.noreply.github.com>
2024-02-27 08:48:51 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ fetch('https://my-json-server.typicode.com/rupeshrauniyar/film/film')
     .then(data => {
         var clutter = "";
         data.forEach(function(data, index) {
-            clutter += `<div class="container" id="${index}">
+            clutter += `<a href="watch.html"><div class="container" id="${index}">
                             <div class="img-container">
                                 <img src="${data.img}" alt="">
                             </div>
                             <div class="title">
                                 <h4>${data.title}</h4>
                                 <span>${data.year}</span>
-                            </div>
+                            </div></a>
                         </div>`;
+                
         });
         item.innerHTML = clutter;
-
+        
         // Add click event listener to each container
-        var containers = document.querySelectorAll('.container');
-        containers.forEach(container => {
-        container.addEventListener('click', function(dets) {
+    var containers = document.querySelectorAll('.container');
+    containers.forEach(container => {
+    container.addEventListener('click', function(dets) {
     var iframeSrc = data[dets.currentTarget.id].url;
     window.location.href = 'watch.html?iframeSrc=' + encodeURIComponent(iframeSrc);
+   
 });
 
 });
